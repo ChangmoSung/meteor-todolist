73c5373543af4164744638e2bdb3045b39b5d7f2
Changmo Sung <changmosung@MacBook-Pro.local>
2020-06-20 10:27:21 -0400

    Filtering data with publish and subscribe

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,6 +4,14 @@ import { check } from "meteor/check";
 
 export const Tasks = new Mongo.Collection("tasks");
 
+if (Meteor.isServer) {
+  Meteor.publish("tasks", function tasksPublication() {
+    return Tasks.find({
+      $or: [{ private: { $ne: true } }, { owner: this.userId }],
+    });
+  });
+}
+
 Meteor.methods({
   "tasks.insert"(text) {
     check(text, String);
@@ -16,11 +24,17 @@ Meteor.methods({
       text,
       createdAt: new Date(),
       owner: this.userId,
-      username: Meteor.users.findOne(this.uesrId).username,
+      username: Meteor.users.findOne(this.userId).username,
     });
   },
   "tasks.remove"(taskId) {
     check(taskId, String);
+
+    const task = Tasks.findOne(taskId);
+
+    if (task.private && task.owner !== this.userId) {
+      throw new Meteor.Error("not-authorized");
+    }
 
     Tasks.remove(taskId);
   },
@@ -28,6 +42,24 @@ Meteor.methods({
     check(taskId, String);
     check(setChecked, Boolean);
 
+    const task = Tasks.findOne(taskId);
+
+    if (task.private && task.owner !== this.userId) {
+      throw new Meteor.Error("not-authorized");
+    }
+
     Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+  "tasks.setPrivate"(taskId, setToPrivate) {
+    check(taskId, String);
+    check(setToPrivate, Boolean);
+
+    const task = Tasks.findOne(taskId);
+
+    if (task.owner !== this.userId) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
   },
 });
